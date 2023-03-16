78c623a054fbb4d66cef4892b404ffeadfdb23c0
Artur Z <artur@tcore.io>
2023-03-16 13:08:28 +0100

    aaaaaaaaaa

--- SomerenDAL/StudentDao.cs
+++ SomerenDAL/StudentDao.cs
@@ -9,7 +9,7 @@ namespace SomerenDAL
     {
         public List<Student> GetAllStudents()
         {
-            string query = "SELECT StudentId, Name FROM [TABLE]";
+            string query = "SELECT * FROM [student]";
             SqlParameter[] sqlParameters = new SqlParameter[0];
             return ReadTables(ExecuteSelectQuery(query, sqlParameters));
         }
@@ -17,13 +17,20 @@ namespace SomerenDAL
         private List<Student> ReadTables(DataTable dataTable)
         {
             List<Student> students = new List<Student>();
-
+            int current_student_id = 0;
             foreach (DataRow dr in dataTable.Rows)
             {
+                current_student_id = (int)dr["id"];
+
+                string query = $"SELECT * FROM [human] WHERE id={current_student_id}";
+                SqlParameter[] sqlParameters = new SqlParameter[0];
+                DataRow dt_current_student = ExecuteSelectQuery(query, sqlParameters).Rows[0];
+
                 Student student = new Student()
                 {
-                    Number = (int)dr["StudentId"],
-                    Name = dr["Name"].ToString()
+                    Number = (int)dr["id"],
+                    Name = dt_current_student["first_name"].ToString() + " " +
+                            dt_current_student["last_name"].ToString()
                 };
                 students.Add(student);
             }
