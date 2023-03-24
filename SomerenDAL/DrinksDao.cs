bb6a7c588594ae501aad9468460e33ca52fd38dc
Wouter Altepost <wout.altepost@gmail.com>
2023-03-24 14:29:25 +0100

    Drinks Dao updated

---
+++ SomerenDAL/DrinksDao.cs
@@ -0,0 +1,41 @@
+﻿using SomerenModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class DrinksDao : BaseDao
+    {
+        public List<Drinks> GetAllDrinks()
+        {
+            string query = "SELECT id,name,price,stock, isalcoholic FROM [drinks]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Drinks> ReadTables(DataTable dataTable)
+        {
+            List<Drinks> drink = new List<Drinks>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                drink.Add(
+                    new Drinks()
+                    {
+                        Id = (int)dr["id"],
+                        Name = (string)dr["name"],
+                        Price = (double)dr["price"],
+                        Stock = (int)dr["stock"],
+                        IsAlcoholic = (bool)dr["isalcholic"]
+                    }
+                );;
+            };
+            return drink;
+        }
+    }
+}
---
+++ SomerenModel/Drinks.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drinks
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public double Price { get; set; }
+        public int Stock { get; set; }
+        public bool IsAlcoholic { get; set; } // 0 = alcholic, 1 = non alcoholic
+    }
+
+    enum ListOfDrinks
+    {
+        Fanta, 
+        Coke,
+        Beer,
+        Wine,
+        Water
+    }
+
+    enum DrinkType
+    {
+        Alcoholic = 0,
+        NonAlcoholic = 1
+    }
+}
